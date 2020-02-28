a3e76c0de41b836a2a711f4a348c686dac38a6e8
navkal <navkal@hotmail.com>
2020-02-27 21:51:21 -0500

    Initial commit

---
+++ energizeAndover.py
@@ -0,0 +1,11 @@
+# Copyright 2020 Energize Andover.  All rights reserved.
+
+print( 'Dear JetBrains,' )
+print( '' )
+print( 'Please grant a free PyCharm Pro license' )
+print( 'to the Energize Andover program,' )
+print( 'so we can develop new projects' )
+print( 'using Django.' )
+print( '' )
+print( 'Thank you,' )
+print( 'The Energize Andover Team' )
